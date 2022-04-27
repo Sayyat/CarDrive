7bc8135fee9beb614608e9e73fcb4218d84d31a4
Sayyat <61268689+Sayyat@users.noreply.github.com>
2022-04-27 18:27:24 +0600

    Update CarDrive-HTML5-Shipping.UE4.js

--- CarDrive-HTML5-Shipping.UE4.js
+++ CarDrive-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('CarDrive-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Sayyat/CarDrive/c96035d66e2bf060dfddf79c17db9700dda2cf5f/CarDrive-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
@@ -1154,8 +1154,8 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('CarDrive-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Sayyat/CarDrive/c96035d66e2bf060dfddf79c17db9700dda2cf5f/CarDrive-HTML5-Shipping.data.js'));
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Sayyat/CarDrive/c96035d66e2bf060dfddf79c17db9700dda2cf5f/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('CarDrive-HTML5-Shipping.data')).then(function(dataBlob) {
